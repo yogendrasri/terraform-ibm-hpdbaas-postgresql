ba17b9a1735284da044ac7708c1e345f4e24f5c6
Yogendra Srivastava <ysrivastava@in.ibm.com>
2021-04-22 22:44:34 +0530

    test fail

--- test/stages/stage2-hpdbaas-mongodb.tf
+++ test/stages/stage2-hpdbaas-mongodb.tf
@@ -4,10 +4,10 @@ module "hpdbaas_mongodb" {
   resource_group_name = module.resource_group.name
   resource_location   = var.region
   name_prefix         = "${var.name_prefix}_2"
-  hpcs-resource-group = var.hpcs-resource-group
-  hpcs-region  = var.hpcs-region
-  hpcs-name    = var.hpcs-name
-  hpcs-key     = var.hpcs-key
+  #hpcs-resource-group = var.hpcs-resource-group
+  #hpcs-region  = var.hpcs-region
+  #hpcs-name    = var.hpcs-name
+  #hpcs-key     = var.hpcs-key
   authorize-kms       = var.authorize-kms == "true"
   ibmcloud_api_key    = var.ibmcloud_api_key
 }
