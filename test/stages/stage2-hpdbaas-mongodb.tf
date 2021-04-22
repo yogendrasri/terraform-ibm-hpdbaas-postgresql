e546bcb1c1d188495769b342200f593ee83e043c
Yogendra Srivastava <ysrivastava@in.ibm.com>
2021-04-22 16:49:11 +0530

    test case fixed

--- test/stages/stage2-hpdbaas-mongodb.tf
+++ test/stages/stage2-hpdbaas-mongodb.tf
@@ -1,7 +1,7 @@
 module "dev_tools_mongodb" {
   source = "./module"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.name
   resource_location   = var.region
   name_prefix         = "${var.name_prefix}_2"
   hpcs-region  = var.hpcs-region
